c98815408e0612263036adfc29042e00477a07ea
fernconte <fernandocontento549@gmail.com>
2026-01-23 18:45:07 +0400

    Delete cyberquiz directory

--- cyberquiz/app/api/leaderboard/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { getLeaderboard } from "@/lib/data/store";
-
-export const runtime = "nodejs";
-
-export async function GET() {
-  const items = await getLeaderboard();
-  return NextResponse.json({ items });
-}
